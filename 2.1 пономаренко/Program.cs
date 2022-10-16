2e4367e1be6f2a798dd1d55fd90359a040866509
admin <admin@ADMIN-PC>
2022-10-16 21:10:05 +0200

    Добавьте файлы проекта.

---
+++ 2.1 пономаренко/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите X: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите Y: ");
+double y = double.Parse(Console.ReadLine());
+if (x >= 0 && y >= 0)  Console.WriteLine("Первая четверть");
+else if (x <= 0 && y >= 0) Console.WriteLine("Вторая четверть");
+else if (x <= 0 && y <= 0) Console.WriteLine("Третья четверть");
+else if (x >= 0 && y <= 0) Console.WriteLine("Четвертая четверть");
